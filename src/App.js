b034c3cec88686bc994e077e3fa6fe4192c75a5a
Kausar Akther <kausar.akther@gmail.com>
2020-05-31 00:25:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,24 +11,9 @@ class App extends Component {
     super(props)
     this.state = {}
 
-
-    // console.log("this is my in");
-
-    // const movies = [
-    //   {id: 0, poster_src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfbN7KsqHgs39wOf8EYZh_ZZXaNuFMlLFmJK83cRFsOy5OexArGw&s", title: "Avengers: Infinity War", overview: "Add another layer to your designs with our new duotone icons, available as part"},
-    //   {id: 1, poster_src: "https://i.ytimg.com/vi/Acg6hkEzQqY/maxresdefault.jpg", title: "Avengers", overview: "Get vector icons and social logos on your website with Font Awesome, the web's most popular icon set"},
-
-    // ]
-
     
 
-    // let movieRows = [] 
-    // movies.forEach((movie) => {
-    //   const movieRow = <MovieRow movie={movie}/>
-    //   movieRows.push(movieRow);
-    // })
 
-    // this.state = {rows: movieRows}
 
     this.performSearch();
   }
@@ -39,16 +24,12 @@ performSearch(){
   $.ajax({
     url: urlString,
     success: (searchResults) => {
-      // console.log("Fetchced data successfully")
-      // console.log(searchResults);
       const results = searchResults.results
-      // console.log(results[0])
 
       let movieRows = []
 
       results.forEach((movie) => {
         movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
-        // console.log(movie.title)
         const movieRow = <MovieRow key={movie.id} movie = {movie}/>
         movieRows.push(movieRow)
       })
@@ -56,15 +37,12 @@ performSearch(){
       this.setState({rows: movieRows})
     },
     error: (xhr, status, err) => {
-      // console.error("Failed to fetch data")
-            // console.error("Failed to fetch data")
 
     }
   })
 }
 
 searchChangeHandler(event) {
-  // console.log(event.target.value)
   const boundObject = this
   const searchTerm = event.target.value
   boundObject.performSearch(searchTerm)
