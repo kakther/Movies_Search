a73f13c234e7888eadd263b6fc9eb5c89450d7da
Kausar Akther <kausar.akther@gmail.com>
2020-05-31 01:41:25 -0400

    Updates

---
+++ precache-manifest.b3ac6a68c41045af974819282536dce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3efb15e7c2aa80e177bf805e02311fce",
+    "url": "/movies_search/index.html"
+  },
+  {
+    "revision": "7848208a86fcad771bf4",
+    "url": "/movies_search/static/css/main.89482341.chunk.css"
+  },
+  {
+    "revision": "394eefcbae63fe39a9c3",
+    "url": "/movies_search/static/js/2.fa5d2798.chunk.js"
+  },
+  {
+    "revision": "4a7f45bf2e9127c104c09f9a82b0af0e",
+    "url": "/movies_search/static/js/2.fa5d2798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7848208a86fcad771bf4",
+    "url": "/movies_search/static/js/main.961b1581.chunk.js"
+  },
+  {
+    "revision": "7447e3c2629844d53291",
+    "url": "/movies_search/static/js/runtime-main.5b2b94af.js"
+  }
+]);
